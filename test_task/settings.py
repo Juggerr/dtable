d7f7b955bca980dddb38d6978ad1507664a0f522
Ihor Liakh <lyakhigor@gmail.com>
2014-08-11 19:25:48 +0000

    setted debug = false

--- test_task/settings.py
+++ test_task/settings.py
@@ -20,9 +20,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 SECRET_KEY = '=-3j1vu1$cpu*3)ah1db49+jkiwiud3om+3kahqhdtk)-x_go5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-TEMPLATE_DEBUG = True
+TEMPLATE_DEBUG = False
 
 ALLOWED_HOSTS = []
 
